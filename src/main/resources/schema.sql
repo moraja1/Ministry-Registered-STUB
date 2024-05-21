331fd59fc4038dcbea1bc990eb804a946054ef6c
Jason Mora <jeisonmora0115@gmail.com>
2024-05-21 11:22:49 -0600

    Changed server port on config

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -6,23 +6,16 @@ create table stub_users
 usuario varchar(255),
 primary key (id));
 
-insert into stub_users (usuario) values ("401060606");
-insert into stub_users (usuario) values ("501450255");
-insert into stub_users (usuario) values ("103580495");
-insert into stub_users (usuario) values ("200010002");
-insert into stub_users (usuario) values ("101230321");
-insert into stub_users (usuario) values ("506320985");
-insert into stub_users (usuario) values ("706540321");
-insert into stub_users (usuario) values ("306570841");
-insert into stub_users (usuario) values ("409630258");
-insert into stub_users (usuario) values ("103210654");
-insert into stub_users (usuario) values ("207410987");
-insert into stub_users (usuario) values ("609850236");
-insert into stub_users (usuario) values ("207930197");
-insert into stub_users (usuario) values ("106480258");
-insert into stub_users (usuario) values ("309830642");
-insert into stub_users (usuario) values ("407860142");
-insert into stub_users (usuario) values ("706320522");
-insert into stub_users (usuario) values ("601410252");
-insert into stub_users (usuario) values ("703630141");
-insert into stub_users (usuario) values ("708580141");
+insert into stub_users (usuario) values ("202220222");
+insert into stub_users (usuario) values ("303330333");
+insert into stub_users (usuario) values ("404440444");
+insert into stub_users (usuario) values ("505550555");
+insert into stub_users (usuario) values ("606660666");
+insert into stub_users (usuario) values ("707770777");
+insert into stub_users (usuario) values ("808880888");
+insert into stub_users (usuario) values ("909990999");
+insert into stub_users (usuario) values ("102220222");
+insert into stub_users (usuario) values ("201110111");
+insert into stub_users (usuario) values ("301110111");
+insert into stub_users (usuario) values ("401110111");
+insert into stub_users (usuario) values ("501110111");
